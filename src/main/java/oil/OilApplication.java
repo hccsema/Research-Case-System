8d1744105df35af1fe1111c208bed299bc3325d8
waiter2020 <1403976416@qq.com>
2018-09-17 18:21:23 +0800

    新建项目

---
+++ src/main/java/oil/OilApplication.java
@@ -0,0 +1,12 @@
+package oil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OilApplication.class, args);
+    }
+}
