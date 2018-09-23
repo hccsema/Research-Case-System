d6574a7008449021f066629d864e56dbc1339d84
waiter2020 <1403976416@qq.com>
2018-09-23 23:47:09 +0800

    添加邮件服务

--- src/main/java/oil/controller/UserController.java
+++ src/main/java/oil/controller/UserController.java
@@ -9,11 +9,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -32,6 +34,12 @@ public class UserController {
     @Autowired
     private RoleService roleService;
 
+    /**
+     * 添加用户
+     * @param user
+     * @param model
+     * @return
+     */
     @Transactional(rollbackFor = Exception.class)
     @PostMapping(value = "/add")
     public String addUser(User user, Model model){
@@ -48,6 +56,24 @@ public class UserController {
             user.setNonExpired(true);
             userDetailsService.save(user);
         }
+        return "";
+    }
+
+    /**
+     *修改密码
+     */
+    @Transactional(rollbackFor = Exception.class)
+    @PostMapping(value = "/change_pwd")
+    public String changePassWd(String pwd, HttpServletRequest request,Model model){
+        Assert.notNull(pwd,"密码不为空");
+        Assert.hasLength(pwd,"密码不为空");
+        if (pwd.length()<7){
+            model.addAttribute("msg","密码长度大于6");
+            return "";
+        }
+        String remoteUser = request.getRemoteUser();
+        User byUserName = userDetailsService.findByUserName(remoteUser);
+        byUserName.setPassWord(bCryptPasswordEncoder.encode(pwd));
         return "";
     }
 
